e75e596233378d97386ed83162656feec42a9bc1
Andrii <hlybchenko.andrii@gmail.com>
2022-10-24 00:15:48 +0300

    Moments of inertia for isosceles triangles added.

--- src/main/resources/static/momentOfInertia.js
+++ src/main/resources/static/momentOfInertia.js
@@ -60,13 +60,13 @@ function calcTubeParam(external_dia, inner_dia) {
     document.getElementById('tubePolarResistMomentResult').innerHTML = resultPolarResistMoment;
 }
 
-var last_width_rect = 8;
-var last_height_rect = 12;
+var last_width_rect_triangle = 8;
+var last_height_rect_triangle = 12;
 function calcRectParam(width_rect, height_rect) {
     width_rect = width_rect === 0 ?
-        width_rect = window.last_width_rect : window.last_width_rect = width_rect;
+        width_rect = window.last_width_rect_triangle : window.last_width_rect_triangle = width_rect;
     height_rect = height_rect === 0 ?
-        height_rect = window.last_height_rect : window.last_height_rect = height_rect;
+        height_rect = window.last_height_rect_triangle : window.last_height_rect_triangle = height_rect;
 
     let area = width_rect * height_rect;
     let axialMomentsJx = (width_rect * Math.pow(height_rect, 3))/12;
@@ -83,4 +83,27 @@ function calcRectParam(width_rect, height_rect) {
     document.getElementById('rectAreaResult').innerHTML = resultArea;
     document.getElementById('rectAxialMomentsResult').innerHTML = resultAxialMoments;
     document.getElementById('rectAxialResistMomentsResult').innerHTML = resultResistMoments;
+}
+
+function calcTriangleParam(width_triangle, height_triangle) {
+    width_triangle = width_triangle === 0 ?
+        width_triangle = window.last_width_rect_triangle : window.last_width_rect_triangle = width_triangle;
+    height_triangle = height_triangle === 0 ?
+        height_triangle = window.last_height_rect_triangle : window.last_height_rect_triangle = height_triangle;
+
+    let area = (width_triangle * height_triangle)/2;
+    let axialMomentsJx = (width_triangle * Math.pow(height_triangle, 3))/36;
+    let axialMomentsJy = (height_triangle * Math.pow(width_triangle, 3))/48;
+    let resistMomentsWx = (width_triangle * Math.pow(height_triangle, 2))/24;
+    let resistMomentsWy = (height_triangle * Math.pow(width_triangle, 2))/24;
+
+    let resultArea = "<p>\\[A= {{b * h} \\over 2} = {{"+ width_triangle +" * "+ height_triangle+"} \\over 2} = "+ area.toFixed(3) +"\\]</p>";
+    let resultAxialMoments = "<p>\\[J_x= {{b * h^3} \\over 12} = {{"+ width_triangle +" * "+ height_triangle+"^3} \\over 36} ="+ axialMomentsJx.toFixed(3) +"\\]</p>" +
+        "<p>\\[J_y= {{b^3 * h} \\over 12} = {{"+ width_triangle +"^3 * "+ height_triangle+"} \\over 48} ="+ axialMomentsJy.toFixed(3) +"\\]</p>";
+    let resultResistMoments = "<p>\\[W_x= {{b * h^2} \over 6} = {{"+ width_triangle +" * "+ height_triangle+"^2} \\over 24} ="+ resistMomentsWx.toFixed(3) +"\\]</p> " +
+        "<p>\\[W_y= {{b^2 * h} \\over 6} = {{"+ width_triangle +"^2 * "+ height_triangle+"} \\over 24} ="+ resistMomentsWy.toFixed(3) +"\\]</p>";
+
+    document.getElementById('triangleAreaResult').innerHTML = resultArea;
+    document.getElementById('triangleAxialMomentsResult').innerHTML = resultAxialMoments;
+    document.getElementById('triangleAxialResistMomentsResult').innerHTML = resultResistMoments;
 }
